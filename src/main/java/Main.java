b94a51a237a65390905219344bb19fdc1b3ca7aa
Aidan Lawrence <aidanmatthewlawrence@gmail.com>
2023-11-16 14:53:23 -0600

    Final Version M1 CT

---
+++ src/main/java/Main.java
@@ -0,0 +1,29 @@
+/* PSEUDOCODE
+Objective: Write 5 System.out.println() statements with the following information:
+First Name
+Last Name
+Street Address
+City
+Zip Code
+
+Structure:
+Program Start
+	print : Harry
+	print : Potter
+	print : 4 Privet Drive
+	print : Little Whinging
+	print : 64075
+Program End
+ */
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Harry");
+        System.out.println("Potter");
+        System.out.println("4 Privet Drive");
+        System.out.println("Little Whinging");
+        System.out.println("64075");
+
+    }
+}
